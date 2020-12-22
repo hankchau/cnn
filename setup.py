dcc762d413853bc627af8dc055bae32cc6af7020
hankchau <hankchau@umich.edu>
2020-12-22 18:17:42 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cnn',
+    install_requires=[
+        'bs4==0.01',
+        'numpy==1.19.4',
+        'matplotlib==3.3.3',
+        'pillow==8.0.1',
+        'tensorflow==2.4.0'
+    ],
+)
